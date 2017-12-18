58d76fa2d7533b69575e9bc9723d9c987b1c1601
Aleksey Stepanenko <f1ex@inbox.ru>
2017-12-18 22:15:57 +0300

    init commit

---
+++ 1.6.7-EchoServer_with_threads/server.py
@@ -0,0 +1,14 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 1234))
+s.listen(10)
+while True:
+    conn, addr = s.accept()
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        if data.decode('ascii').strip() == "close":
+            break
+        conn.send(data)
+    conn.close()
