fa808722b9b84f998faa8fe00ce110eaf1982a3e
Aleksey Stepanenko <f1ex@inbox.ru>
2018-01-01 16:28:14 +0300

    add unicorn app

---
+++ 1.9.11_gunicorn_app/hello.py
@@ -0,0 +1,18 @@
+import pprint
+
+# 'PATH_INFO': '/hello',
+# 'QUERY_STRING': 'a=12&b=1&b=2',
+
+def app(environ, start_response):
+    # print(environ)
+    pprint.pprint(environ)
+    vars = environ['QUERY_STRING'].split("&")
+    data = '\n'.join(vars) + '\n'
+    data = data.encode('utf-8')
+    # data = b"Hello, World!\n"
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+#        ("Content-Length", str(len(data)))
+    ])
+
+    return iter([data])
